b6a5ec1982f6f319c94ac88dc5134aa37bccec9b
Chandrakanta Bhoi <chandrakantabhoi10@gmail.com>
2025-05-06 14:48:48 +0530

    Add  permission classes to require authenticated users

--- backend/settings.py
+++ backend/settings.py
@@ -207,7 +207,10 @@ PAYPAL_SECRET_ID = env('PAYPAL_SECRET_ID')
 REST_FRAMEWORK = {
     'DEFAULT_AUTHENTICATION_CLASSES': (
         'rest_framework_simplejwt.authentication.JWTAuthentication',
-    )
+    ),
+    'DEFAULT_PERMISSION_CLASSES': (
+        'rest_framework.permissions.IsAuthenticated',
+    ),
 }
 
 ANYMAIL = {
